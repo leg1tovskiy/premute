1f0b72eb1333207772a847d36d8243c31e4587b7
leg1tovskiy <84225262+leg1tovskiy@users.noreply.github.com>
2026-09-17 00:31:11 +0900

    Add files via upload

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -68,6 +68,7 @@ export type ModRow = {
   name: string;
   steamid: string;
   discord: string | null;
+  avatar: string | null;
   rank: number | null;
   norma: { week: number; month: number } | null;
   bans: number | null;
